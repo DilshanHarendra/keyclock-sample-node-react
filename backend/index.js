eab65eddb1251bec78376db4dd7edc46533a47d4
DilshanHarendra <harendracripto@gmail.com>
2022-12-21 15:31:15 +0530

    connect keyclock

--- backend/index.js
+++ backend/index.js
@@ -2,35 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const productRouter = require('./routes/products');
-const jwt = require('jsonwebtoken')
+const session = require('express-session');
+const {keycloak,memoryStore} = require('./keyclockConfig');
 
 app.use(express.json());
 app.use(cors());
 
 
+
+// // Configure session
+app.use(
+    session({
+        secret: 'mySecret',
+        resave: false,
+        saveUninitialized: true,
+        store: memoryStore,
+    })
+);
+
+
+app.use( keycloak.middleware() );
+
+
+
+
 app.use('/api/products', productRouter);
 
-const authMiddleware = (req,res,next) =>{
-    const authorization = req.headers.authorization
-    if (authorization){
-        const token = authorization.split(" ")[1]||''
-        if (token){
-            try {
-                jwt.verify(token,'admin',{ algorithms: 'RS256'})
-            }catch (e) {
-                res.status(401).send({message:e})
-            }
 
-        }else{
-            res.status(401).send({message:'no token'})
-        }
-    }else {
-        res.status(401).send({message:'no token'})
-    }
-    res.send()
-}
-
-app.get('/public',authMiddleware,(req, res) => {
+app.get('/public',(req, res) => {
     res.send( {'date': new Date() } )
 });
 
