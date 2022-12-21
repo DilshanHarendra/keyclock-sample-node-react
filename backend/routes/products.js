8a169a7f1effedaa72f915c38c86e9a2b11482d1
DilshanHarendra <harendracripto@gmail.com>
2022-12-21 15:31:24 +0530

    add middleware

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,18 +1,18 @@
 const express = require('express');
-
+const {keycloak} = require('../keyclockConfig')
 // initialize router
 const router = express.Router();
 const products =[]
 
-router.get('/', (req, res) => {
+router.get('/',keycloak.protect('realm:read_access'), (req, res) => {
     res.status(200).send({data:products})
 });
-router.get('/:id', (req, res) => {
+router.get('/:id',keycloak.protect('realm:read_access'), (req, res) => {
     const id= req.params.id
     res.status(200).send({data:products[id]})
 });
 
-router.post('/', (req, res) => {
+router.post('/',keycloak.protect('realm:write_access'), (req, res) => {
     const product ={
         ...req.body,
         id: products.length + 1
